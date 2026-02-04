3093d0cec405ef2d28ca570b264035dd2d70bdf3
Jay Lhomme <jhgatohagire@gmail.com>
2026-02-03 21:21:44 -0600

    Second Commit

--- src/my_logger.py
+++ src/my_logger.py
@@ -1,2 +1,32 @@
 import logging
-f
+from pathlib import Path
+
+Path("logs").mkdir(exist_ok=True)
+
+
+def get_logger(name: str = "basic_logger") -> loggin.logger:
+    logger = logging.getlogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+
+        file_handler = logging.FileHandler("logs/basic.log", mode="a")
+        file_handler.setLevel(logging.ERROR)
+
+        console_format = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        file_format = logging.Formatter(
+            "{'time':'%(asctime)s', 'name': '%(name)s', "
+            "'level': '%(levelname)s', 'message': '%(message)s'}"
+        )
+
+        console_handler.setFormatter(console_format)
+        file_handler.setFormatter(file_format)
+
+        logger.addHandler(console_handler)
+        logger.addhandler(file_handler)
+
+    return logger
